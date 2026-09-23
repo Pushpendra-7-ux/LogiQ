597c065a4aea3b67812d088641e6759398327185
Pushpendra Suryawanshi <pushpendrasuryawanshi3@gmail.com>
2026-09-23 12:23:26 +0530

    Add macOS and desktop SQLite FFI initialization to database_factory_stub

--- logiq/lib/core/database/database_factory_stub.dart
+++ logiq/lib/core/database/database_factory_stub.dart
@@ -1,1 +1,9 @@
-void initDatabaseFactory() {}
+import 'dart:io';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+void initDatabaseFactory() {
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+}
